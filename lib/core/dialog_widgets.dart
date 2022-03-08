0f5087abf3a1b58bb58ae11cdcf69c6d72157d62
sj <sj@xp.com>
2022-03-08 13:33:41 +0800

    refactor yes icon widget codes

--- lib/core/dialog_widgets.dart
+++ lib/core/dialog_widgets.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dialog_shower/broker/broker.dart';
 import 'package:flutter_dialog_shower/view/rotate_widget.dart';
 import 'dialog_wrapper.dart';
 
@@ -27,12 +28,12 @@ class DialogWidgets {
   }
 
   static DialogShower showSuccess({Widget? icon, String? text = 'Success', bool dismissible = true}) {
-    Widget? widget = icon ?? iconSuccess;
+    Widget? widget = icon ?? iconSuccess ?? CcWidgetUtils.getOneSuccessWidget();
     return showTips(icon: widget, text: text)..barrierDismissible = dismissible;
   }
 
   static DialogShower showLoading({Widget? icon, String? text = 'Loading', bool dismissible = false}) {
-    Widget widget = RotateWidget(child: icon ?? iconLoading ?? GappyCirclePainter.getOneGappyCircle());
+    Widget widget = RotateWidget(child: icon ?? iconLoading ?? CcWidgetUtils.getOneGappyCircle());
     return showTips(icon: widget, text: text)..barrierDismissible = dismissible;
   }
 
@@ -64,17 +65,14 @@ class DialogWidgets {
       ),
     );
 
-    // rewrite properties
+    // rewrite properties cause show actually in the next micro task :)
     dialog
       ..barrierDismissible = true
       ..alignment = Alignment.center
       ..barrierColor = Colors.black.withAlpha(64)
       ..transitionDuration = const Duration(milliseconds: 200)
       ..transitionBuilder = (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
-        return ScaleTransition(
-          child: child,
-          scale: Tween(begin: 0.0, end: 1.0).animate(animation),
-        );
+        return ScaleTransition(child: child, scale: Tween(begin: 0.0, end: 1.0).animate(animation));
       };
     return dialog;
   }
@@ -232,6 +230,105 @@ class DialogWidgets {
   }
 }
 
+/// Convenient Widget for being easy to use
+class CommonStatefulWidget<T extends CommonStatefulWidgetState> extends StatefulWidget {
+  CommonStatefulWidget({Key? key, this.builder, this.initState, this.dispose}) : super(key: key);
+
+  void Function(T state)? initState;
+  void Function(T state)? dispose;
+  Widget Function(T state, BuildContext context)? builder;
+
+  @override
+  T createState() => CommonStatefulWidgetState() as T;
+}
+
+class CommonStatefulWidgetState extends State<CommonStatefulWidget> {
+  @override
+  void initState() {
+    widget.initState?.call(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.dispose?.call(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    assert(widget.builder != null, 'Cannot provide a null builder !!!');
+    return widget.builder!(this, context);
+  }
+}
+
+class CommonStatefulWidgetWithTicker extends CommonStatefulWidget {
+  CommonStatefulWidgetWithTicker({Key? key, builder, initState, dispose})
+      : super(key: key, builder: builder, initState: initState, dispose: dispose);
+
+  @override
+  CommonStatefulWidgetWithTickerState createState() => CommonStatefulWidgetWithTickerState();
+}
+
+class CommonStatefulWidgetWithTickerState extends CommonStatefulWidgetState with SingleTickerProviderStateMixin {}
+
+class CommonStatefulWidgetWithTickers extends CommonStatefulWidget {
+  CommonStatefulWidgetWithTickers({Key? key, builder, initState, dispose})
+      : super(key: key, builder: builder, initState: initState, dispose: dispose);
+
+  @override
+  CommonStatefulWidgetWithTickersState createState() => CommonStatefulWidgetWithTickersState();
+}
+
+class CommonStatefulWidgetWithTickersState extends CommonStatefulWidgetState with TickerProviderStateMixin {}
+
+/// Painters
+// https://blog.codemagic.io/flutter-custom-painter/
+// https://github.com/sbis04/custom_painter
+// https://medium.com/flutter-community/playing-with-paths-in-flutter-97198ba046c8
+// https://github.com/divyanshub024/flutter_path_animation
+class YesIconPainter extends CustomPainter {
+  double width, height;
+  double? stroke;
+  Color? color;
+  double? progress;
+
+  YesIconPainter({required this.width, required this.height, this.stroke, this.color, this.progress = 1.0});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = stroke ?? 25
+      ..color = color ?? Colors.white;
+
+    if (progress != null) {
+      double p = progress!;
+
+      // the best ratio is width: height is 3 : 2
+      Path path = Path();
+      double shortLineX = 0;
+      double shortLineY = height / 2;
+      double shortLineW = width / 3;
+
+      path.moveTo(shortLineX, shortLineY);
+
+      double ratio = p * 2;
+      double ratioShort = ratio <= 1 ? ratio : 1, ratioLong = ratio >= 1 ? ratio - 1 : 0;
+
+      // draw short line & long line
+      path.lineTo(shortLineX + (shortLineW - shortLineX) * ratioShort, shortLineY + (height - shortLineY) * ratioShort);
+      if (ratioLong != 0) {
+        path.lineTo(shortLineW + (width - shortLineW) * ratioLong, height - ratioLong * height);
+      }
+      canvas.drawPath(path, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant YesIconPainter oldDelegate) => oldDelegate.progress != progress;
+}
+
 class GappyCirclePainter extends CustomPainter {
   double radius;
   double strokeWidth;
@@ -270,14 +367,46 @@ class GappyCirclePainter extends CustomPainter {
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
 
+class CcWidgetUtils {
   static Widget getOneGappyCircle({double? side, double? stroke}) {
     side ??= 64.0;
     stroke ??= 4.0;
     return Container(
-        width: side,
-        height: side,
-        alignment: Alignment.center,
-        child: CustomPaint(painter: GappyCirclePainter(radius: side / 2, strokeWidth: stroke)));
+      width: side,
+      height: side,
+      alignment: Alignment.center,
+      child: CustomPaint(painter: GappyCirclePainter(radius: side / 2, strokeWidth: stroke)),
+    );
+  }
+
+  static Widget getOneSuccessWidget() {
+    return Container(
+      width: 90,
+      height: 90,
+      padding: const EdgeInsets.only(left: 5, top: 10),
+      child: CommonStatefulWidgetWithTicker(
+        initState: (state) {
+          AnimationController _controller = AnimationController(vsync: state, duration: const Duration(milliseconds: 500));
+          Broker.setIfAbsent<AnimationController>(_controller, key: '_key_of_yes_animation_controller_');
+          _controller.forward();
+        },
+        dispose: (state) {
+          Broker.remove<AnimationController>('_key_of_yes_animation_controller_')?.dispose();
+        },
+        builder: (state, context) {
+          AnimationController? _controller = Broker.get(key: '_key_of_yes_animation_controller_');
+          assert(_controller != null, '_controller cannot be null, should init in initState');
+          return AnimatedBuilder(
+              animation: _controller!,
+              builder: (context, snapshot) {
+                return CustomPaint(
+                  painter: YesIconPainter(width: 90, height: 60, stroke: 16, progress: _controller.value),
+                );
+              });
+        },
+      ),
+    );
   }
 }
