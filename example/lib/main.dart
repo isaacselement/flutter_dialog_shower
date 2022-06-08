f015aad722b64aab7057c3d8448ff987a677265a
sj <sj@xp.com>
2022-06-08 14:10:32 +0800

    fixed demo issue

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'app.dart';
 
 void main() {
+  debugPaintSizeEnabled = true;
+  debugPaintPointersEnabled = true;
+  debugRepaintRainbowEnabled = true;
+  debugPaintLayerBordersEnabled = true;
+
   runApp(const App());
 }
