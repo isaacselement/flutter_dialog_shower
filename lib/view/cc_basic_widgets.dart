e220d1a4afb20f1750e7649b9015e7aaac182a9e
sj <sj@xp.com>
2022-06-16 14:28:01 +0800

    forbid multi tap CcTapOnceWidget

--- lib/view/cc_basic_widgets.dart
+++ lib/view/cc_basic_widgets.dart
@@ -40,3 +40,49 @@ class CcTapWidgetState extends State<CcTapWidget> {
     );
   }
 }
+
+class CcTapOnceWidget extends StatefulWidget {
+  final Widget? child;
+  final Widget Function(bool isTapping)? builder;
+  final void Function(CcTapOnceWidgetState state) onTap;
+
+  const CcTapOnceWidget({Key? key, this.builder, this.child, required this.onTap}) : super(key: key);
+
+  @override
+  State<CcTapOnceWidget> createState() => CcTapOnceWidgetState();
+}
+
+class CcTapOnceWidgetState extends State<CcTapOnceWidget> {
+  bool _isTapingDown = false;
+
+  get isTapingDown => _isTapingDown;
+
+  set isTapingDown(v) {
+    setState(() => _isTapingDown = v);
+  }
+
+  bool _isTappedOnce = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Listener(
+      onPointerDown: (e) => isTapingDown = true,
+      onPointerUp: (e) => isTapingDown = false,
+      onPointerCancel: (e) => isTapingDown = false,
+      child: GestureDetector(
+        child: widget.builder?.call(isTapingDown) ??
+            Opacity(
+              opacity: isTapingDown ? 0.5 : 1,
+              child: widget.child,
+            ),
+        onTap: () {
+          if (_isTappedOnce) {
+            return;
+          }
+          _isTappedOnce = true;
+          widget.onTap(this);
+        },
+      ),
+    );
+  }
+}
